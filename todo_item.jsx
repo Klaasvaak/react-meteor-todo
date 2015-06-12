bdea0afdf381da8f189161816f45f28437fac530
Joep van der Heijden <jvdheijden90@gmail.com>
2015-06-12 10:44:44 +0200

    Reformat code, add animation, add editable item

--- todo_item.jsx
+++ todo_item.jsx
@@ -2,44 +2,78 @@ var cx = React.addons.classSet;
 
 TodoItem = React.createClass({
 
-    // TodoApp is the state owner and should be the one to manipulate the data
-    // We pass the event handlers through TodoList to TodoApp
-    handleToggle: function() {
-        this.props.onToggle(this.props.task._id);
-    },
-    handleRemove: function() {
-        this.props.onRemove(this.props.task._id);
-    },
+  getInitialState: function () {
+    return {
+      edit: false
+    };
+  },
 
-    render: function() {
-        var createdAt = this.props.task.createdAt.toString()
-        var checkedButtonLabel = this.props.task.done ?
-            (<i className="checkmark icon large "></i>) :
-            (<i className="checkmark icon large disabled"></i>);
+  // TodoApp is the state owner and should be the one to manipulate the data
+  // We pass the event handlers through TodoList to TodoApp
+  handleToggle: function () {
+    this.props.onToggle(this.props.task._id);
+  },
 
-        // Define the css classes using the classSet addon.
-        var itemClasses = cx("ui task item", this.props.task.done ? 'done' : '')
-        var segmentClasses = cx("ui segment",
-            this.props.task.important ? "red" : "");
-        var textClasses = cx("ui middle aligned header",
-            this.props.task.done ? "disabled" : "");
-        var removeClasses = cx("middle aligned ui right floated icon remove \
+  handleRemove: function () {
+    this.props.onRemove(this.props.task._id);
+  },
+
+  openEdit: function () {
+    this.setState({
+      edit: true
+    });
+  },
+
+  closeEdit: function() {
+    this.props.onEdit(this.props.task._id, this.refs.edit.getDOMNode().value);
+
+    this.setState({
+      edit: false
+    });
+  },
+
+  componentDidUpdate: function () {
+    if (this.state.edit) {
+      this.refs.edit.getDOMNode().focus();
+    }
+  },
+
+  render: function () {
+    var createdAt = this.props.task.createdAt.toString();
+    var checkedButtonLabel = this.props.task.done ?
+      (<i className="checkmark icon large "></i>) :
+      (<i className="checkmark icon large disabled"></i>);
+
+    // Define the css classes using the classSet addon.
+    var itemClasses = cx("ui task item", this.props.task.done ? 'done' : '');
+    var segmentClasses = cx("ui segment",
+      this.props.task.important ? "red" : "");
+    var textClasses = cx("ui middle aligned header",
+      this.props.task.done ? "disabled" : "");
+    var removeClasses = cx("middle aligned ui right floated icon remove \
             large", this.props.task.done ? "disabled" : "");
 
-        return (
-            <div className={itemClasses}>
-                <div className={segmentClasses}>
-                    <div className="ui ribbon label small"
-                        onClick={this.handleToggle}>
-                        {checkedButtonLabel}
-                    </div>
-                    <span className={textClasses}>
-                        {this.props.task.text}
-                    </span>
-                    <i className={removeClasses} onClick={this.handleRemove}></i>
-                </div>
-            </div>
-        )
+    var textHTML = '';
+    if (!this.state.edit) {
+      textHTML = this.props.task.text;
+    } else {
+      textHTML = <input ref="edit" onBlur={this.closeEdit} defaultValue={this.props.task.text}/>;
     }
+
+    return (
+      <div className={itemClasses}>
+        <div className={segmentClasses}>
+          <div className="ui ribbon label small"
+               onClick={this.handleToggle}>
+            {checkedButtonLabel}
+          </div>
+          <span className={textClasses} onClick={this.openEdit}>
+              {textHTML}
+          </span>
+          <i className={removeClasses} onClick={this.handleRemove}></i>
+        </div>
+      </div>
+    )
+  }
 
 });
--- todo_list.jsx
+++ todo_list.jsx
@@ -1,29 +1,34 @@
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
 TodoList = React.createClass({
 
-    render: function() {
-        var _this = this;
-        var todoItemsNodes = this.props.tasks.map(function (task) {
+  render: function () {
+    var _this = this;
+    var todoItemsNodes = this.props.tasks.map(function (task) {
 
-            // We pass the event handlers fromt TodoItem to TodoApp
-            return (
-                <TodoItem task={task} onToggle={_this.props.onToggle} onRemove={_this.props.onRemove}/>
-            );
-        });
-        return todoItemsNodes.length > 0 ? (
-            <div className="todoList ui items">
-                {todoItemsNodes}
-            </div>
-        ) : (
-            <div className="ui icon message">
-                <i className="birthday icon"></i>
-                <div className="content">
-                    <div className="header">
-                        Awesome!
-                    </div>
-                    <p>You have completed all your tasks!</p>
-                </div>
-            </div>
-        );
-    }
+      // We pass the event handlers from TodoItem to TodoApp
+      return (
+        <TodoItem key={task._id} task={task} onEdit={_this.props.onEdit} onToggle={_this.props.onToggle}
+                  onRemove={_this.props.onRemove}/>
+      );
+    });
+    return todoItemsNodes.length > 0 ? (
+      <div className="todoList ui items">
+        <ReactCSSTransitionGroup transitionName="todoitem" transitionAppear={true}>
+          {todoItemsNodes}
+        </ReactCSSTransitionGroup>
+      </div>
+      ) : (
+      <div className="ui icon message">
+        <i className="birthday icon"></i>
+        <div className="content">
+          <div className="header">
+            Awesome!
+          </div>
+          <p>You have completed all your tasks!</p>
+        </div>
+      </div>
+    );
+  }
 
 });
